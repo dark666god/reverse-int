0743475839e166a76629de9cac8a4a85ebe15e94
Vitaliy Chistyakov <dark666god@yandex.ru>
2023-02-11 13:41:16 +0300

    fead:create test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    function reverseInt(n) {
+        const reversed = n.toString().split('').reverse().join('')
+        return parseInt(reversed) * Math.sign(n)
+      }
 }
