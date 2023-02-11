238ee12a2743c9b4d72cb10ed35acd228f141bca
Vitaliy Chistyakov <dark666god@yandex.ru>
2023-02-11 15:35:58 +0300

    fead:done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-module.exports = function reverse (n) {
-    function reverseInt(n) {
-        const reversed = n.toString().split('').reverse().join('')
-        return parseInt(reversed) * Math.sign(n)
-      }
-}
+module.exports = function reverse(x) {
+    if (x < 0)
+    return Number((x * -1).toString().split("").reverse().join("")); // тут я возвращаю число х умноженное на -1 что дает мне переводить отрицательные числа в положительные и наоборот,
+    return Number(x.toString().split("").reverse().join(""));
+};
+// function reverse(s){
+//     return s.split("").reverse().join("");
+// } https://stackoverflow.com/questions/958908/how-do-you-reverse-a-string-in-place-in-javascript
